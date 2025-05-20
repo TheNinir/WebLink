7514bc96a6b1e86cddb05282f01e040f197c33fa
TheNinir <marecekninir@seznam.cz>
2025-05-20 20:44:44 +0200

    TODO: hide/show

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './Card.jsx';
 import pic from './assets/Alucard.jpg';
 import Card from './Card.jsx';
 
-
 function App() {
+  const a = useRef();
   return (
     <>
-      <Card img = {pic} url = {1} />;
+      <Card id="cardID" img = {pic} page = {1} />
+      <div className='inputDIV'>
+      <input ref={a} className='urlInput' type='text' defaultValue={1}></input>
+      <br></br>
+      <button className='newCardBTN'  onClick={()=>{console.log()}} >CREATE CARD</button>
+      </div>
       
-    </>
+      </>
   )
 }
 
